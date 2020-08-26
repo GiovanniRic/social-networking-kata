52373c79a48699cada5980dfed9b12ed4a14c91e
GiovanniRic <gio.riccio90@gmail.com>
2020-08-26 23:29:08 +0200

    control duplicate follow before save

--- src/main/java/claranet/italia/social/networking/kata/repository/FollowKataRepository.java
+++ src/main/java/claranet/italia/social/networking/kata/repository/FollowKataRepository.java
@@ -3,17 +3,21 @@ package claranet.italia.social.networking.kata.repository;
 import java.util.List;
 
 public class FollowKataRepository implements FollowRepository {
-	
+
 	private Storage<String> persistence;
-	
-	 public FollowKataRepository() {
-		 persistence = new StorageKataFollow();
+
+	public FollowKataRepository() {
+		persistence = new StorageKataFollow();
 	}
 
 	@Override
 	public void saveFollow(String user, String anotherUser) {
-		persistence.save(user, anotherUser);
-		
+
+		List<String> followUser = getFollowFor(user);
+		if (!followUser.stream().anyMatch(follow -> follow.equals(anotherUser))) {
+
+			persistence.save(user, anotherUser);
+		}
 	}
 
 	@Override
